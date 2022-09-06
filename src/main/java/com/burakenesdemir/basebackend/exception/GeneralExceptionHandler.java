d3214f1319301c5e39c0e4148a181ff8f164ec89
burakenesdemir <bburakenesdemir@gmail.com>
2022-09-06 21:48:16 +0300

    Created based exception handling class

---
+++ src/main/java/com/burakenesdemir/basebackend/exception/GeneralExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.burakenesdemir.basebackend.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String logMessage = "\n Class : {}\n Line : {}\n Message : {}";
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+                                                          HttpHeaders headers,
+                                                          HttpStatus status,
+                                                          WebRequest request){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        log.error(logMessage, e.getStackTrace()[0].getClassName(), e.getStackTrace()[0].getLineNumber(), e.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<Object> unAuthorizedExceptionHandler(UnAuthorizedException e) {
+        log.error(logMessage, e.getStackTrace()[0].getClassName(), e.getStackTrace()[0].getLineNumber(), e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}

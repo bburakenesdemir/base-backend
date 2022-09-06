7de707d080075f69eda17bb184a94f749dbee916
Burak Enes Demir <burakenesdemir@192.168.1.93>
2022-09-06 20:59:39 +0300

    Initial commit

---
+++ src/main/java/com/burakenesdemir/basebackend/BaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.burakenesdemir.basebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseBackendApplication.class, args);
+	}
+
+}
